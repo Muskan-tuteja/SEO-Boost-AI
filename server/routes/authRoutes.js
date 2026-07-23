812d09453dae62cdea1f660b0d934f8250232651
Muskan Tuteja <muskantuteja714@gmail.com>
2026-07-23 15:37:38 +0530

    creted the folder authRotes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,3 +1,11 @@
 import express from "express";
+import { login,registerUser } from "../controllers/authController.js";
+import auth from "../middleware/auth.js";
+
 
 const authRoutes = express.Router();
+
+authRoutes.post("/register", registerUser);
+authRoutes.post("/login", login);
+authRoutes.get('/user', auth,getUser);
+export default authRoutes;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import "dotenv/config";
 import dns from 'dns';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 // DNS fix — connectDB() se PEHLE set karo
 dns.setServers(['8.8.8.8', '8.8.4.4']);
@@ -16,6 +17,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {    
